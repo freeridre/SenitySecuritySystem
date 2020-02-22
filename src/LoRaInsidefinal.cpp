2f8cb916d0fd89bf023fe217a357bad312af74cf
freeridre <pastersofmupets@gmail.com>
2020-02-22 18:16:19 +0100

    Tons of bug fix, changed frequency

--- src/LoRaInsidefinal.cpp
+++ src/LoRaInsidefinal.cpp
@@ -2,22 +2,23 @@
 #include <HardwareSerial.h>
 #include "heltec.h"
 
-#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
+#define BAND 868E6   //433E6  //you can set band here directly,e.g. 868E6,915E6
 HardwareSerial MEGA(1);
-uint8_t CardDataFromKapu[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t CardDataFromKapu[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 uint8_t byteFromKapu;
 uint8_t CARDDATATOINSIDEMEGA;
-int BackFromSenityStatus;
+uint8_t BackFromSenityStatus;
 int packetSize = LoRa.parsePacket();
 boolean ReceiveDataFromLoRa();
 void SendCardDataToArduino();
 void ReceiveDataFromArduino();
+void GetControlDatacheckFromOutSideLoRa();
 void setup() {
     //WIFI Kit series V1 not support Vext control
   //Serial.begin(115200);
   MEGA.begin(115200,SERIAL_8N1, 36, 12); //RX, TX
   Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
-  
+  LoRa.setTxPowerMax(20);
   Serial.println();
   Serial.println("Hello It's LoRaInSide.");
   Serial.println("Waiting For InComing Data...");
@@ -56,6 +57,20 @@ boolean ReceiveDataFromLoRa() {
       //Serial.println(byteFromKapu.charAt(4));
       i++;
     }
+    /*if (CardDataFromKapu[0] == 1 && CardDataFromKapu[1] == 1 && CardDataFromKapu[2] == 1)
+    {
+      Serial.println(CardDataFromKapu[0]);
+      Serial.println(CardDataFromKapu[1]);
+      Serial.println(CardDataFromKapu[2]);
+      LoRa.beginPacket();
+      LoRa.write(BackFromSenityStatus);
+      LoRa.endPacket();
+      ret = false;
+      return ret;
+    }else if(CardDataFromKapu[0] == 2 && CardDataFromKapu[1] == 2 && CardDataFromKapu[2] == 2)
+    {
+      BackFromSenityStatus = 0;
+    }*/
     // print RSSI of packet
     //Read DATA From LoRa
     for (int i = 0; i < sizeof(CardDataFromKapu); i++)
@@ -71,24 +86,36 @@ boolean ReceiveDataFromLoRa() {
 
 void ReceiveDataFromArduino() {
 
-  uint8_t availableData = MEGA.available();
+  //uint8_t availableData = MEGA.available();
   /*Serial.print("Available data: ");
   Serial.println(availableData, HEX);*/
             
   
-  while(MEGA.available() > 0)
+  while(MEGA.available())
   {    
     BackFromSenityStatus = MEGA.read();
     Serial.print("Received Data From InSide Arduino: ");
     Serial.println(BackFromSenityStatus, DEC);
+    if (BackFromSenityStatus == 112)
+    {
+      Serial.println("LoRa Inside Has Just Reseted! Cause of Arduino Inside Reset!");
+      BackFromSenityStatus = 0;
+      ESP.restart();
+    }
+    //delay(250);
     LoRa.beginPacket();
-    Serial.println("Send Recieved Data From Arduino to Gate LoRa.");
+    //Serial.print("Send Recieved Data ");
+    //Serial.print(BackFromSenityStatus);
+    //Serial.println("From Arduino to Gate LoRa.");
     //MEGA.println("Send Data to Gate LoRa");
+    Serial.print("Send Received Data "); Serial.print(BackFromSenityStatus); Serial.println(" From Arduino to Gate LoRa.");
+    //LoRa.write(BackFromSenityStatus);
     LoRa.write(BackFromSenityStatus);
     LoRa.endPacket();
     Serial.println("End Data Sending to Gate LoRa");
     //MEGA.println("End Data Sending to Gate LoRa");
   }
+  BackFromSenityStatus = 0;
 }
 
 
@@ -100,4 +127,8 @@ void SendCardDataToArduino()
       MEGA.write(CardDataFromKapu[i]);
   }
 }
-  
+void GetControlDatacheckFromOutSideLoRa()
+{
+  int ControlPacket = LoRa.parsePacket();
+
+}
